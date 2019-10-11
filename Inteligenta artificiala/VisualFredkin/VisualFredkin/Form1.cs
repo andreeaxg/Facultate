46099ce13e8199caf49dc01e5f11eec05072584b
andreeaxg <51415777+andreeaxg@users.noreply.github.com>
2019-10-11 13:29:58 +0300

    Add files via upload

---
+++ Inteligenta artificiala/VisualFredkin/VisualFredkin/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VisualFredkin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Engine.Init(pictureBox2);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e) //NextStep
+        {
+            Engine.Step();
+        }
+
+        private void button2_Click(object sender, EventArgs e) //InfiniteSteps
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Engine.Step();
+                Update();
+            }
+        }
+    }
+}
